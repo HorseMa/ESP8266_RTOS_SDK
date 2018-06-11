b8c6da2d454055149020b6c60d94213bfe312cb8
HorseMa <wei_technology@163.com>
2018-06-11 20:33:06 +0800

    wake up & sleep is ok; buzzer is ok

--- examples/tgam_demo/user/user_main.c
+++ examples/tgam_demo/user/user_main.c
@@ -28,6 +28,17 @@
 #include "uart.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "c_types.h"
+#include "esp8266/ets_sys.h"
+
+static xTaskHandle key_handler_task_handle;
+void tgam_enter_sleep(void);
+void user_gpio_init(void);
+void TGAM_powerenable(void);
+void TGAM_powerdisable(void);
+void buzzer_enable(void);
+void buzzer_disable(void);
+void button_enable(void);
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -135,6 +146,38 @@ void led_task(void  *pvParameters)
     vTaskDelete(NULL);
 }
 
+void key_task(void  *pvParameters)
+{
+    while(1)
+    {
+        //os_printf("key task\r\n");
+        vTaskSuspend( NULL );
+        uart_init_new();
+        buzzer_enable();
+        vTaskDelay(200 / portTICK_RATE_MS);
+        buzzer_disable();
+        os_printf("wakeup callback !!!!!!!!!\r\n");
+        user_gpio_init();
+        button_enable();
+        TGAM_powerenable();
+        wifi_set_opmode(STATION_MODE);
+
+        struct station_config config;
+        bzero(&config, sizeof(struct station_config));
+        sprintf(config.ssid, SSID);
+        sprintf(config.password, PASSWORD);
+        wifi_station_set_config(&config);
+
+        wifi_set_event_handler_cb(wifi_event_handler_cb);
+
+        wifi_station_connect();
+        vTaskSuspend( NULL );
+        tgam_enter_sleep();
+    }
+    os_printf("key task delete !!!!!!!!!!!!\r\n");
+    vTaskDelete(NULL);
+}
+
 void user_gpio_init(void)
 {
     //引脚功能设置
@@ -169,25 +212,40 @@ void buzzer_disable(void)
     GPIO_OUTPUT_SET(5, 0);
 }
 
+static void gpio_intr_handler()
+{
+    uint32 gpio_status;
+
+    _xt_isr_mask(1<<ETS_GPIO_INUM);    //disable interrupt
+    gpio_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
+    if(gpio_status & BIT(12))//判断中断针脚
+    {
+        //printf("go sleep !!!!\n");
+        //tgam_enter_sleep();
+        xTaskResumeFromISR( key_handler_task_handle );
+    }
+    GPIO_REG_WRITE( GPIO_STATUS_W1TC_ADDRESS, gpio_status ); //clear interrupt mask
+    _xt_isr_unmask(1 << ETS_GPIO_INUM); //Enable the GPIO interrupt
+}
+
+void button_enable(void)
+{
+    GPIO_ConfigTypeDef pGPIOConfig;
+
+    gpio_pin_wakeup_disable();
+    pGPIOConfig.GPIO_Pin = GPIO_Pin_12;
+    pGPIOConfig.GPIO_Mode = GPIO_Mode_Input;
+    pGPIOConfig.GPIO_Pullup = GPIO_PullUp_DIS;
+    pGPIOConfig.GPIO_IntrType = GPIO_PIN_INTR_NEGEDGE;
+    gpio_config(&pGPIOConfig);
+
+    gpio_intr_handler_register(gpio_intr_handler,NULL);
+    GPIO_REG_WRITE( GPIO_STATUS_W1TC_ADDRESS, 0xFFFFFFFF ); //clear interrupt mask
+    _xt_isr_unmask(1 << ETS_GPIO_INUM);    //Enable the GPIO interrupt
+}
+
 void fpm_wakup_cb_func(void)
 {
-    uart_init_new();
-    os_printf("wakeup callback !!!!!!!!!\r\n");
-    user_gpio_init();
-    TGAM_powerenable();
-    //buzzer_enable();
-    xTaskCreate(led_task,  "led_task", 256,    NULL,   2,  NULL);
-    wifi_set_opmode(STATION_MODE);
-
-    struct station_config config;
-    bzero(&config, sizeof(struct station_config));
-    sprintf(config.ssid, SSID);
-    sprintf(config.password, PASSWORD);
-    wifi_station_set_config(&config);
-
-    wifi_set_event_handler_cb(wifi_event_handler_cb);
-
-    wifi_station_connect();
     return;
 }
 
@@ -204,6 +262,13 @@ void tgam_enter_sleep(void)
     PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12); //key
     gpio_pin_wakeup_enable(12,GPIO_PIN_INTR_LOLEVEL);
     wifi_fpm_set_wakeup_cb(fpm_wakup_cb_func);     // Set wakeup callback
+    buzzer_enable();
+    vTaskDelay(200 / portTICK_RATE_MS);  //send every 1 seconds
+    buzzer_disable();
+    vTaskDelay(200 / portTICK_RATE_MS);  //send every 1 seconds
+    buzzer_enable();
+    vTaskDelay(200 / portTICK_RATE_MS);  //send every 1 seconds
+    buzzer_disable();
     wifi_fpm_do_sleep(0xFFFFFFF);
 }
 
@@ -216,6 +281,9 @@ void tgam_enter_sleep(void)
 void user_init(void)
 {
     uart_init_new();
+    button_enable();
+    xTaskCreate(led_task,  "led_task", 256,    NULL,   2,  NULL);
+    xTaskCreate(key_task,  "key_task", 256,    NULL,   2,  &key_handler_task_handle);
     os_printf("SDK version:%s %d\n", system_get_sdk_version(), system_get_free_heap_size());
     tgam_enter_sleep();
 }
